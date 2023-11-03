6d89343a8c1d92fd7d73f5a1ca89984f7c8e658d
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-11-03 18:57:28 +0100

    Mise à jour de l'interface utilisateur : - Alignement des boutons horizontalement. - Amélioration de l'espacement pour une meilleure lisibilité. - Ajout d'un bouton pour vider les champs de saisie (Clear Inputs). - Amélioration de la gestion des résultats avec un bouton pour effacer les résultats précédents (Clear Results).

--- main.py
+++ main.py
@@ -5,31 +5,38 @@ def conScan(tgtHost, tgtPort):
     try:
         connskt = socket(AF_INET, SOCK_STREAM)
         connskt.connect((tgtHost, tgtPort))
-        result_text.insert(tk.END, f'[+] {tgtPort}/tcp open\n')
+        result_list.insert(tk.END, f'[+] {tgtPort}/tcp open')
         connskt.close()
     except:
-        result_text.insert(tk.END, f'[-] {tgtPort}/tcp closed\n')
+        result_list.insert(tk.END, f'[-] {tgtPort}/tcp closed')
 
 def portScan():
     tgtHost = host_entry.get()
     tgtPorts = port_entry.get().split(',')
-    result_text.delete(1.0, tk.END)
+    result_list.delete(0, tk.END)
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
-        result_text.insert(tk.END, f'[-] Cannot resolve {tgtHost}\n')
+        result_list.insert(tk.END, f'[-] Cannot resolve {tgtHost}')
         return
 
     try:
         tgtName = gethostbyaddr(tgtIP)
-        result_text.insert(tk.END, f'\n[+] Scan result of: {tgtName[0]}\n')
+        result_list.insert(tk.END, f'\n[+] Scan result of: {tgtName[0]}')
     except:
-        result_text.insert(tk.END, f'\n[+] Scan result of: {tgtIP}\n')
+        result_list.insert(tk.END, f'\n[+] Scan result of: {tgtIP}')
 
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
-        result_text.insert(tk.END, f'Scanning Port: {tgtPort}\n')
+        result_list.insert(tk.END, f'Scanning Port: {tgtPort}')
         conScan(tgtHost, int(tgtPort))
+
+def clear_results():
+    result_list.delete(0, tk.END)
+
+def clear_inputs():
+    host_entry.delete(0, tk.END)
+    port_entry.delete(0, tk.END)
 
 def on_closing():
     root.destroy()
@@ -37,38 +44,39 @@ def on_closing():
 root = tk.Tk()
 root.title("Port Scanner")
 
-# Calcul des dimensions de la fenêtre
-window_width = 400
+window_width = 500
 window_height = 400
 
-# Obtenir les dimensions de l'écran
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-# Calculer la position pour centrer la fenêtre
 x = (screen_width // 2) - (window_width // 2)
 y = (screen_height // 2) - (window_height // 2)
 
-# Définir les dimensions de la fenêtre et la positionner au centre
 root.geometry(f'{window_width}x{window_height}+{x}+{y}')
 
 host_label = tk.Label(root, text="Target Host (IP or Hostname):")
-host_label.pack()
+host_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
 host_entry = tk.Entry(root)
-host_entry.pack()
+host_entry.grid(row=0, column=1, padx=10, pady=10)
 
 port_label = tk.Label(root, text="Target Ports (comma separated):")
-port_label.pack()
+port_label.grid(row=1, column=0, padx=10, pady=10, sticky='w')
 
 port_entry = tk.Entry(root)
-port_entry.pack()
+port_entry.grid(row=1, column=1, padx=10, pady=10)
 
 scan_button = tk.Button(root, text="Scan Ports", command=portScan)
-scan_button.pack()
+clear_results_button = tk.Button(root, text="Clear Results", command=clear_results)
+clear_inputs_button = tk.Button(root, text="Clear Inputs", command=clear_inputs)
 
-result_text = tk.Text(root, height=15, width=50)
-result_text.pack()
+scan_button.grid(row=2, column=0, padx=10, pady=10)
+clear_results_button.grid(row=2, column=1, padx=10, pady=10)
+clear_inputs_button.grid(row=2, column=2, padx=10, pady=10)
+
+result_list = tk.Listbox(root, height=15, width=70)
+result_list.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
