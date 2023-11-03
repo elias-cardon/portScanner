560c3e45cf657c68bfcb2c6523475d65b310992c
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-11-03 18:36:35 +0100

    On ajoute une interface graphique

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
+import tkinter as tk
 from socket import *
 
 def conScan(tgtHost, tgtPort):
     try:
-        # Crée un objet de socket avec la famille d'adresses IPv4 et le type de socket en mode flux
         connskt = socket(AF_INET, SOCK_STREAM)
-        # Tente de se connecter à l'adresse IP ou au nom d'hôte et au numéro de port spécifiés
         connskt.connect((tgtHost, tgtPort))
-        # Affiche un message indiquant que le port est ouvert si la connexion réussit
-        print('[+]%d/tcp open' % tgtPort)
-        # Ferme la connexion
+        result_text.insert(tk.END, f'[+] {tgtPort}/tcp open\n')
         connskt.close()
     except:
-        # Affiche un message indiquant que le port est fermé si la connexion échoue
-        print('[-]%d/tcp closed' % tgtPort)
+        result_text.insert(tk.END, f'[-] {tgtPort}/tcp closed\n')
 
-def portScan(tgtHost, tgtPorts):
+def portScan():
+    tgtHost = host_entry.get()
+    tgtPorts = port_entry.get().split(',')
+    result_text.delete(1.0, tk.END)
     try:
-        # Obtient l'adresse IP de la cible en utilisant son nom d'hôte
         tgtIP = gethostbyname(tgtHost)
     except:
-        # Affiche un message d'erreur si l'adresse IP de la cible ne peut pas être résolue
-        print('[-] Cannot resolve %s ' % tgtHost)
+        result_text.insert(tk.END, f'[-] Cannot resolve {tgtHost}\n')
         return
 
     try:
-        # Obtient le nom d'hôte associé à l'adresse IP
         tgtName = gethostbyaddr(tgtIP)
-        # Affiche le nom d'hôte si disponible
-        print('\n[+] Scan result of: %s' % tgtName[0])
+        result_text.insert(tk.END, f'\n[+] Scan result of: {tgtName[0]}\n')
     except:
-        # Affiche l'adresse IP si le nom d'hôte n'est pas disponible
-        print('\n[+] Scan result of: %s' % tgtIP)
+        result_text.insert(tk.END, f'\n[+] Scan result of: {tgtIP}\n')
 
-    # Configure le temps d'attente pour les connexions à 1 seconde
     setdefaulttimeout(1)
-
     for tgtPort in tgtPorts:
-        # Affiche un message indiquant le port en cours de numérisation
-        print('Scanning Port: %d' % tgtPort)
-        # Appelle la fonction conScan pour scanner le port en cours
+        result_text.insert(tk.END, f'Scanning Port: {tgtPort}\n')
         conScan(tgtHost, int(tgtPort))
 
-if __name__ == '__main__':
-    # Appelle la fonction portScan pour scanner les ports 80 et 22 de la machine cible 'google.com'
-    portScan('google.com', [80, 22])
+def on_closing():
+    root.destroy()
+
+root = tk.Tk()
+root.title("Port Scanner")
+
+# Calcul des dimensions de la fenêtre
+window_width = 400
+window_height = 400
+
+# Obtenir les dimensions de l'écran
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculer la position pour centrer la fenêtre
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+
+# Définir les dimensions de la fenêtre et la positionner au centre
+root.geometry(f'{window_width}x{window_height}+{x}+{y}')
+
+host_label = tk.Label(root, text="Target Host (IP or Hostname):")
+host_label.pack()
+
+host_entry = tk.Entry(root)
+host_entry.pack()
+
+port_label = tk.Label(root, text="Target Ports (comma separated):")
+port_label.pack()
+
+port_entry = tk.Entry(root)
+port_entry.pack()
+
+scan_button = tk.Button(root, text="Scan Ports", command=portScan)
+scan_button.pack()
+
+result_text = tk.Text(root, height=15, width=50)
+result_text.pack()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+root.mainloop()
